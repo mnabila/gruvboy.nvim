49ec3377bc933eb5f342e1750faf7f54f13541b4
M. Nabil Adani <nblid48@gmail.com>
2021-10-09 06:46:15 +0700

    refactor: update hightlight for nvim-cmp

--- lua/gruvboy/plugins/cmp.lua
+++ lua/gruvboy/plugins/cmp.lua
@@ -1,10 +1,13 @@
 local lush = require("lush")
 local base = require("gruvboy.base")
+local c = require("gruvboy.colors")
 
 local theme = lush(function()
     return {
         CmpDocumentation({ base.NormalFloat }),
         CmpDocumentationBorder({ base.FloatBorder }),
+        CmpItemAbbrDefault({ fg = c.fg2 }),
+        CmpItemKindDefault({ fg = c.yellow }),
     }
 end)
 
