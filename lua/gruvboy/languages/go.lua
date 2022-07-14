44085e68f85e4d878a624ad353702c2f24ca5f2a
M. Nabil Adani <nblid48@gmail.com>
2022-07-14 22:11:01 +0700

    feat: add new hl-group for golang

--- lua/gruvboy/languages/go.lua
+++ lua/gruvboy/languages/go.lua
@@ -2,13 +2,14 @@ local lush = require("lush")
 local c = require("gruvboy.colors")
 
 local theme = lush(function()
-    return {
-        goDirective({ fg = c.aqua }),
-        goConstants({ fg = c.purple }),
-        goDeclaration({ fg = c.red }),
-        goDeclType({ fg = c.blue }),
-        goBuiltins({ fg = c.orange }),
-    }
+	return {
+		goDirective({ fg = c.aqua }),
+		goConstants({ fg = c.purple }),
+		goDeclaration({ fg = c.red }),
+		goDeclType({ fg = c.blue }),
+		goBuiltins({ fg = c.orange }),
+		goParen({ fg = c.purple }),
+	}
 end)
 
 return theme
