2b86844ce1fda0445797eab1b87ab99f7779ad6b
M. Nabil Adani <nblid48@gmail.com>
2022-10-27 21:02:26 +0700

    feat: add support for symbols-outline.nvim

--- lua/gruvboy/plugins/init.lua
+++ lua/gruvboy/plugins/init.lua
@@ -7,5 +7,7 @@ theme = utils.merge(theme, require("gruvboy.plugins.gitsigns"))
 theme = utils.merge(theme, require("gruvboy.plugins.telescope"))
 -- translator.nvim hightlight group
 theme = utils.merge(theme, require("gruvboy.plugins.translator"))
+-- simrat39/symbols-outline.nvim highlight group
+theme = utils.merge(theme, require("gruvboy.plugins.symbols"))
 
 return theme
---
+++ lua/gruvboy/plugins/symbols.lua
@@ -0,0 +1,10 @@
+local c = require("gruvboy.colors")
+
+local theme = {
+	FocusedSymbol = { bg = c.yellow, fg = c.bg0 },
+	SymbolsOutlineConnector = { fg = c.blue },
+}
+
+return theme
+
+-- vi:nowrap
